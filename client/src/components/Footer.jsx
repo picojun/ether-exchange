216bee8f564017eff6959cb680477d551be402c9
jwms <willia32@stolaf.edu>
2023-01-30 21:06:29 -0500

    add footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,49 @@
+import logo from "../../e2.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full flex md:justify-center justify-between flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <img src={logo} alt="logo" className="w-8" />
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full text-white">
+          <p>Market</p>
+          <p>Exchange</p>
+          <p>Tutorials</p>
+          <p>Login</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-col mt-5">
+        <p className="text-white text-xs text-center opacity-70">
+          Get in touch
+          <br />
+          <a
+            href="https://www.github.com/luztambor"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            info@e-squared.com
+          </a>
+        </p>
+      </div>
+      <div className="w-full h-[0.25px] bg-gray-400 mt-5" />
+      <div className="w-full flex justify-between items-center mt-3">
+        <p className="text-white text-xs text-center opacity-70">
+          <a
+            href="https://www.github.com/luztambor"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            made by luztambor
+          </a>
+        </p>
+        <p className="text-white text-xs text-center opacity-70">
+          All rights reserved
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -74,20 +74,21 @@ const Transactions = () => {
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ? (
-          <h3 className="text-white text-3xl text-center my-2">
-            Latest Transactions
-          </h3>
+          <>
+            <h3 className="text-white text-3xl text-center my-2">
+              Latest Transactions
+            </h3>
+            <div className="flex flex-wrap justify-center items-center mt-10">
+              {dummyData.reverse().map((transaction, i) => (
+                <TransactionCard key={i} {...transaction} />
+              ))}
+            </div>
+          </>
         ) : (
           <h3 className="text-white text-2xl text-center my-2">
             Connect a wallet to see transactions here.
           </h3>
         )}
-
-        <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, i) => (
-            <TransactionCard key={i} {...transaction} />
-          ))}
-        </div>
       </div>
     </div>
   );
